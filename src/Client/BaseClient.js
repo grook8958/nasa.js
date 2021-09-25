9b7c5390f5da8b09bdaac27f73f22f46e7e33ae8
grook8958 <clement.puks@gmail.com>
2021-09-25 21:48:34 +0200

    Created BaseClient and Client

--- src/Client/BaseClient.js
+++ src/Client/BaseClient.js
@@ -1,6 +1,29 @@
-
+const { EventEmitter } = require('events');
+const { apiKey } = require('../Utils/Constants');
 
 /**
  * The Base of the Client to interact with the NASA APIs
+ * @extends EventEmitter
  */
-class BaseClient {}
+class BaseClient extends EventEmitter {
+
+    /**
+     * The constructor of this BaseClient
+     * @param {apiKey} apiKey A key to get an advance access to the API, by default it will use the DEMO_KEY. 
+     * Get your own key at https://api.nasa.gov/index.html#signUp
+     */
+    constructor(apiKey) {
+        super();
+
+        /**
+         * The API key to use by the API
+         * @type {apiKey}
+         * @default DEMO_KEY
+         */
+        this.apiKey = apiKey ?? 'DEMO_KEY';
+    }
+
+};
+
+//Export the class
+module.exports = BaseClient;
--- src/Client/Client.js
+++ src/Client/Client.js
@@ -5,7 +5,11 @@ const BaseClient = require('./BaseClient');
 /**
  * The Client used to interact with the NASA APIs
  */
-class Client extends BaseClient {}
+class Client extends BaseClient {
+    constructor() {
+        
+    }
+}
 
 //Export this class
 module.exports = Client;
